c191a18e81881679bf72b37c3bb3b1e8ae7cb1a0
wyrd <wyrd@wyrdnoMacBook-Pro.local>
2020-11-18 08:06:00 +0900

    test

---
+++ BubbleSort.rb
@@ -0,0 +1,32 @@
+def swap(array, idx1, idx2)
+	temp = array[idx1]
+	array[idx1] = array[idx2]
+	array[idx2] = temp
+end
+
+def bubbleSort(array, n)
+	for i in 1..n
+		for j in 1..n-i
+			if array[j-1] > array[j]
+				swap(array, j-1, j)
+			end
+		end
+	end
+end
+
+def disp(array)
+	for i in 0...array.length
+		printf "%d ", array[i]
+	end
+	puts
+end
+
+a1 = [ 3, 2, 9, 6, 1, 8, 4, 7, 5 ]
+a2 = [ 7, -2, -9, 5, 0, 1, -1, -5, 4 ]
+disp(a1)
+bubbleSort(a1, a1.length)
+disp(a1)
+
+disp(a2)
+bubbleSort(a2, a2.length)
+disp(a2)
---
+++ QuickSort.rb
@@ -0,0 +1,46 @@
+def swap(array, idx1, idx2)
+	temp = array[idx1]
+	array[idx1] = array[idx2]
+	array[idx2] = temp
+end
+
+def quickSort(array, left, right)
+	if left >= right
+		return
+	end
+	x = array[(left + right) / 2]
+    pl = left
+    pr = right
+	while pl <= pr
+		while array[pl] < x
+			pl += 1
+		end
+		while array[pr] > x
+			pr -= 1
+		end
+		if pl <= pr
+			swap(array, pl, pr)
+			pl += 1
+			pr -= 1
+		end
+	end
+	quickSort(array, left, pr)
+	quickSort(array, pl, right)
+end
+
+def disp(array)
+	for i in 0...array.length
+		printf "%d ", array[i]
+	end
+	puts
+end
+
+a1 = [ 3, 2, 9, 6, 1, 8, 4, 7, 5 ]
+a2 = [ 7, -2, -9, 5, 0, 1, -1, -5, 4 ]
+disp(a1)
+quickSort(a1, 0, a1.length - 1)
+disp(a1)
+
+disp(a2)
+quickSort(a2, 0, a2.length - 1)
+disp(a2)
